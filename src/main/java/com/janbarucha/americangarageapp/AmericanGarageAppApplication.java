96a212e19c621add0a32479ae0070e8a3d274f53
JanBarucha <100784471+JanBarucha@users.noreply.github.com>
2022-09-15 22:40:28 +0200

    Initial commit

---
+++ src/main/java/com/janbarucha/americangarageapp/AmericanGarageAppApplication.java
@@ -0,0 +1,13 @@
+package com.janbarucha.americangarageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericanGarageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericanGarageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janbarucha/americangarageapp/AmericanGarageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janbarucha.americangarageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericanGarageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
